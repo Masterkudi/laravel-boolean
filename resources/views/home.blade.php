bc9649de4f2d023aada97159c654c49af16500d1
masterkudi <cudini.andrea@gmail.com>
2023-10-19 17:34:57 +0200

    modifica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,4 +8,7 @@
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia quo voluptatibus cupiditate. Ullam, quae id provident dicta aliquam numquam beatae voluptatem quaerat in obcaecati maxime accusamus excepturi porro, nostrum vero!</p>
 >>>>>>> 7f71e7ace20cdecd1945dbb2c756992ff0c66c8f
+
+ciao
+
 @endsection
