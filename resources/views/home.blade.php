c771ce0783bcb9e892b264bd2a554863e66b7922
paolo flore <paoloflore937@gmail.com>
2023-10-19 17:45:12 +0200

    origin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,10 @@
 @extends("layouts.public")
 
 @section("content")
-<<<<<<< HEAD
     <h1>Hello Folks!</h1>
 
-=======
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia quo voluptatibus cupiditate. Ullam, quae id provident dicta aliquam numquam beatae voluptatem quaerat in obcaecati maxime accusamus excepturi porro, nostrum vero!</p>
->>>>>>> 7f71e7ace20cdecd1945dbb2c756992ff0c66c8f
 
 ciao
 
