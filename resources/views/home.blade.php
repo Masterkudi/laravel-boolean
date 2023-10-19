7f71e7ace20cdecd1945dbb2c756992ff0c66c8f
paolo flore <paoloflore937@gmail.com>
2023-10-19 17:25:43 +0200

    lorem

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,4 +2,5 @@
 
 @section("content")
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia quo voluptatibus cupiditate. Ullam, quae id provident dicta aliquam numquam beatae voluptatem quaerat in obcaecati maxime accusamus excepturi porro, nostrum vero!</p>
 @endsection
