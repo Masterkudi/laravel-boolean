4c69a26e8486d934e4fb74bed7ca9c4fcca49db8
paolo flore <paoloflore937@gmail.com>
2023-10-24 17:10:09 +0200

    controller + migrate

--- app/Models/Cocktail.php
+++ app/Models/Cocktail.php
@@ -15,11 +15,7 @@ class Cocktail extends Model
         'category',
         'ingredients',
         'price',
-<<<<<<< HEAD
         'image',
-=======
-        'allergens'
->>>>>>> 690b3a1bc00e8e8bb099324366a45d3bd6b6aa92
 
     ];
 
--- database/migrations/2023_10_20_153208_create_cocktail_table.php
+++ database/migrations/2023_10_20_153208_create_cocktail_table.php
@@ -1,1 +1,25 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
+return new class extends Migration
+{
+
+// Run the migrations.*/
+  public function up(): void{Schema::create('cocktails', function (Blueprint $table) {$table->id();
+
+            $table->string("nome", 100);
+            $table->string("category", 100)->nullable();
+            $table->json("ingredients")->nullable();
+            $table->decimal('price', 8, 2)->default(0.00);
+            $table->text('image');
+            $table->timestamps();
+        });
+    }
+
+    
+
+// Reverse the migrations.
+  public function down(){Schema::dropIfExists('cocktails');}
+};
